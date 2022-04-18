9577857c2fff1b76339b0555aa485fb894546a40
mrpwei <mrpengwei@outlook.com>
2022-04-18 21:20:04 +0800

    movie list滚动功能

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -2,17 +2,39 @@ import {
   ArrowBackIosOutlined,
   ArrowForwardIosOutlined,
 } from "@material-ui/icons";
-import React from "react";
+import React, { useRef, useState } from "react";
 import ListItem from "../ListItem/ListItem";
 import "./list.scss";
 
 const List = () => {
+  const [sliderNumber, setSliderNumber] = useState(0);
+  const [isMoved, setIsMoved] = useState(false);
+  const listRef = useRef();
+  const handleClick = (direction) => {
+    setIsMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === "left" && sliderNumber > 0) {
+      setSliderNumber(sliderNumber - 1);
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+    if (direction === "right" && sliderNumber < 5) {
+      setSliderNumber(sliderNumber + 1);
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    }
+  };
   return (
     <div className="list">
       <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
-        <ArrowBackIosOutlined className="sliderArrow left" />
-        <div className="container">
+        <ArrowBackIosOutlined
+          className="sliderArrow left"
+          onClick={() => handleClick("left")}
+          style={{ display: !isMoved && "none" }}
+        />
+        <div className="container" ref={listRef}>
+          <ListItem />
+          <ListItem />
+          <ListItem />
           <ListItem />
           <ListItem />
           <ListItem />
@@ -21,7 +43,10 @@ const List = () => {
           <ListItem />
           <ListItem />
         </div>
-        <ArrowForwardIosOutlined className="sliderArrow right" />
+        <ArrowForwardIosOutlined
+          className="sliderArrow right"
+          onClick={() => handleClick("right")}
+        />
       </div>
     </div>
   );
